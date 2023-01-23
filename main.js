3aaef4b72e5a30a09b50545d33d11c8127c98ead
Luke <19198075+LukGH4@users.noreply.github.com>
2023-01-22 23:03:00 -0700

    Make func-buttonsPressed a separate function

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const display = document.querySelector('.display');
+const operation = document.querySelector('.operation');
+const decimal = document.querySelector('.decimal');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+
+const digitButtons = [...document.getElementsByClassName('digit')];
+const functionButtons = [...document.getElementsByClassName('func')];
+
+const saveDigitStyle = decimal.style.backgroundColor;
+const savedStyle = functionButtons[1].style.backgroundColor;
+
+let cleared = true;
+let equaled = false;
+let numOnePressed = false;
+let numTwo = 0;
+
+
 function addition (numOne, numTwo) {
     return Math.round((numOne + numTwo) * 10000) / 10000;
 }
@@ -30,53 +48,7 @@ function operate (symbol, numOne, numTwo) {
     }
 }
 
-
-function main () {
-    const digitButtons = [...document.getElementsByClassName('digit')];
-    const functionButtons = [...document.getElementsByClassName('func')];
-    const display = document.querySelector('.display');
-    const operation = document.querySelector('.operation');
-    const clear = document.querySelector('.clear');
-    const equals = document.querySelector('.equals');
-    let cleared = true;
-    let equaled = false;
-    let numOnePressed = false;
-    let numTwo = 0;
-
-    for (let i = 0; i < digitButtons.length; i++) {
-        digitButtons[i].onclick = () => {
-            if (cleared) {
-                if (digitButtons[i].innerHTML !== '.'){
-                display.innerHTML = digitButtons[i].innerHTML;
-                cleared = false;
-                }
-            } else if (numOnePressed && cleared) {
-                if (display.innerHTML.length < 20) {
-                    display.innerHTML = digitButtons[i].innerHTML;
-                };
-            } else {
-                if (display.innerHTML.length < 20) {
-                    display.innerHTML += digitButtons[i].innerHTML;
-                };
-            };
-
-            if (equaled && !numOnePressed) {
-                operation.innerHTML = '';
-                display.innerHTML = digitButtons[i].innerHTML;
-                equaled = false;
-            };
-        };
-    };
-
-    clear.onclick = () => {
-        cleared = true;
-        numOnePressed = false;
-        display.innerHTML = '0';
-        operation.innerHTML = '';
-        display.innerHTML = digitButtons[i].innerHTML;
-        equaled = false;
-    };
-
+function funcButtonPress (functionButtons) {
     for (let i = 0; i < functionButtons.length; i++){
         functionButtons[i].onclick = () => {
             let numOne = Number(display.innerHTML);
@@ -84,9 +56,7 @@ function main () {
             operation.innerHTML = `${numOne} ${functionButtons[i].innerHTML}`;
             numOnePressed = true;
             cleared = true;
-            let savedStyle = functionButtons[i].style.backgroundColor;
             functionButtons[i].style.backgroundColor = 'goldenrod';
-
             functionButtons.map(func => {
                 if (func !== functionButtons[i]) {
                 func.style.backgroundColor = savedStyle;
@@ -107,7 +77,11 @@ function main () {
                     cleared = true;
                     numOnePressed = false;
                     equaled = true;
-                    functionButtons.forEach(func => func.style.backgroundColor = savedStyle);
+                    functionButtons.forEach(func => {
+                        func.style.backgroundColor = savedStyle;
+                    });
+                    funcButtonPress(functionButtons);
+                    decimal.style.backgroundColor = saveDigitStyle;
                 }; 
             };
 
@@ -116,13 +90,72 @@ function main () {
                 numOnePressed = false;
                 numOne = 1;
                 display.innerHTML = '0';
-                functionButtons.forEach(func => func.style.backgroundColor = savedStyle);
+                functionButtons.forEach(func => {
+                    func.style.backgroundColor = savedStyle;
+                });
+                funcButtonPress(functionButtons);
+                decimal.style.backgroundColor = saveDigitStyle;
                 operation.innerHTML = '';
                 equaled = false;
             };
 
         };
     };
+}
+
+
+function main () {
+
+    for (let i = 0; i < digitButtons.length; i++) {
+        digitButtons[i].onclick = () => {
+            if (cleared) {
+                if (digitButtons[i].innerHTML !== '.'){
+                    display.innerHTML = digitButtons[i].innerHTML;
+                    cleared = false;
+                }
+                if (numOnePressed) {
+                    decimal.style.backgroundColor = saveDigitStyle;
+                    functionButtons.map(func => {
+                        if (func.style.backgroundColor !== 'goldenrod') {
+                            func.style.backgroundColor = 'cyan';
+                        };
+                        func.onclick = '';
+                    });
+                }
+            } else {
+                if (display.innerHTML.length < 20) {
+                    if (digitButtons[i].innerHTML == '.' && !display.innerHTML.includes('.')){
+                        display.innerHTML += digitButtons[i].innerHTML;
+                        digitButtons[i].style.backgroundColor = '#D5FFF1';
+                    } else if (digitButtons[i].innerHTML !== '.'){
+                        display.innerHTML += digitButtons[i].innerHTML;
+                    };
+                };
+            };
+
+            if (equaled && !numOnePressed) {
+                if (digitButtons[i].innerHTML !== '.'){
+                    operation.innerHTML = '';
+                    display.innerHTML = digitButtons[i].innerHTML;
+                    cleared = false;
+                    equaled = false;
+                }
+            };
+        };
+    };
+
+    clear.onclick = () => {
+        cleared = true;
+        numOnePressed = false;
+        display.innerHTML = '0';
+        operation.innerHTML = '';
+        decimal.style.backgroundColor = saveDigitStyle;
+        equaled = false;
+        
+    };
+
+    funcButtonPress(functionButtons);
+
 
 };
 
